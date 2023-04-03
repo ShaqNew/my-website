baf5f71620f84cd11f94a3c02c99d7569a764dc3
ShaqNew <shaquillenewell@gmail.com>
2023-04-03 11:36:37 +0100

    Adding further content
    - Added logo images
    - Changed parallax image
    - Formatted Skills text
    - Added further mobile styling

--- src/Components/Skills/SkillsDisplay/SkillButtonContent.tsx
+++ src/Components/Skills/SkillsDisplay/SkillButtonContent.tsx
@@ -1,26 +1,5 @@
 import React from "react";
 
-export const ExperienceContent = () => {
-  return (
-    <div className="experience">
-      <p>
-        With over three years of experience working in projects for top brands
-        such as Calvin Klein, Tommy Hilfiger, British American Tobacco, Tempur
-        Mattress, and The NHS, I have the skills and expertise to excel as a
-        frontend developer and beyond.
-      </p>
-
-      <p>
-        Throughout my career, I have had the opportunity to work with a range of
-        senior and peer-level developers, allowing me to develop a deep
-        understanding of how to work collaboratively in a team environment. I am
-        confident that my experience, knowledge, and passion for web development
-        would make me a valuable addition to a development team.
-      </p>
-    </div>
-  );
-};
-
 export const StackContent = () => {
   return (
     <div className="stackContent">
@@ -48,20 +27,41 @@ export const StackContent = () => {
   );
 };
 
+export const ExperienceContent = () => {
+  return (
+    <div className="experience">
+      <p>
+        With over <b>three</b> years of experience working in projects for top brands
+        such as <b>Calvin Klein</b>, <b>Tommy Hilfiger</b>, <b>British American Tobacco</b>, <b>Tempur
+        Mattress</b>, and The <b>NHS</b>, I have the skills and expertise to excel as a
+        frontend developer and beyond.
+      </p>
+
+      <p>
+        Throughout my career, I have had the opportunity to work with a range of
+        senior and peer-level developers, allowing me to develop a deep
+        understanding of how to work <b>collaboratively</b> in a team environment. I am
+        confident that my experience, knowledge, and passion for web development
+        would make me a valuable addition to a development team.
+      </p>
+    </div>
+  );
+};
+
 export const Outlook = () => {
   return (
     <div className="outlookContent">
       <p>
         As a frontend developer, I believe that striving to always do my best
-        and deliver the cleanest, most functional, and well-tested code is a
+        and deliver the <b>cleanest</b>, most <b>functional</b>, and <b>well-tested</b> code is a
         crucial mindset to have.
       </p>
       <p>
         It not only ensures the success of the project at hand, but also sets a
         standard for future projects and builds a reputation for high-quality
-        work that will only improve as I gain more experience. By prioritizing
-        code cleanliness and functionality, I am able to create a better user
-        experience and make it easier for other developers to work with my code
+        work that will only <b>improve</b> as I gain more experience. By prioritizing
+        code cleanliness and functionality, I am able to create a <b>better user
+        experience</b> and make it easier for other developers to work with my code
         in the future.
       </p>
     </div>
