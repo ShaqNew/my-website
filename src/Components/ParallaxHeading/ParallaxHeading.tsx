004fd92e8007720eeb0391c195a418ea2a796970
ShaqNew <shaquillenewell@gmail.com>
2023-04-17 12:01:34 +0100

    Minor UI improvements
    Added border to buttons
    Made interactive elements clearer
    Reduced animations

--- src/Components/ParallaxHeading/ParallaxHeading.tsx
+++ src/Components/ParallaxHeading/ParallaxHeading.tsx
@@ -5,18 +5,18 @@ import './parallaxHeading.scss'
 const ParallaxHeading = (props:any) => {
     const {image, text} = props;
     return (
-        <div className="PHImage">
+        // <div className="PHImage">
             <ParallaxBanner
             layers={[{ image: image, speed: -15 }]}
             className="aspect-[2/1]"
             >
                 <div className="absolute inset-0 flex items-center justify-center">
-                    <Parallax easing='easeOut' endScroll={-10} scale={[0.01,1.1]}>
+                    <Parallax easing='easeOut' endScroll={-10} scale={[.8,1]}>
                         <h1>{text}</h1>
                     </Parallax>
                 </div>
             </ParallaxBanner>
-        </div>
+        // </div>
     )
 }
 export default ParallaxHeading;
